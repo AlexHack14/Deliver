8c628be7b56b8b4d51a9e189efd46444ae01bca5
Alex Radyuk <43000799+AlexHack14@users.noreply.github.com>
2019-10-18 18:52:27 +0300

    add driver admin& fix some bugs

--- src/main/java/oleksii/radyuk/delivery/controller/DriverController.java
+++ src/main/java/oleksii/radyuk/delivery/controller/DriverController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/drivers")
+@RequestMapping("/driver")
+@CrossOrigin
 public class DriverController {
     @Autowired
     private DriverService driverService;
--- src/main/java/oleksii/radyuk/delivery/controller/TrailerController.java
+++ src/main/java/oleksii/radyuk/delivery/controller/TrailerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/trailer")
+@CrossOrigin
 public class TrailerController {
     @Autowired
     private TrailerService trailerService;
